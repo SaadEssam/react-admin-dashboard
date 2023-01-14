b1d7ddeaebdd3965c71b902c21289b3acce00505
SaadEssam <SaadEssam059@gmail.com>
2023-01-14 16:32:22 +0200

    Team section

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar}/>
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} /> */}
+              <Route path="/team" element={<Team />} />
               {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
--- src/sections/index.js
+++ src/sections/index.js
@@ -1,7 +1,7 @@
 export {default as Topbar} from './global/Topbar';
 export {default as Sidebar} from './global/Sidebar';
 export {default as Dashboard} from './dashboard/dashboard';
-// export {default as Team} from './team/team';
+export {default as Team} from './team/team';
 // export {default as Invoices} from './invoices/invoices';
 // export {default as Contacts} from './contacts/contacts';
 // export {default as Bar} from './bar/bar/';
